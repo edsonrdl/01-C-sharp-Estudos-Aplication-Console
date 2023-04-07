0bcd0f43a5e0b8da170eee6603ed39a905cf2cdb
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2023-04-07 00:28:04 -0300

    feat:Criado tipos de dados personalizados

--- 1.Variáveis e tipos de dados/01-variaveis-e-tipos-de-dados/02-tipos-de-dados-personalizados/02-tipos-de-dados-personalizados.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace _01_variaveis_e_tipos_de_dados
-{
-    public class _02_tipos_de_dados_personalizados
-    {
-    }
-}
---
+++ 1.Variáveis e tipos de dados/01-variaveis-e-tipos-de-dados/02-tipos-de-dados-personalizados/IShape.cs
@@ -0,0 +1,7 @@
+﻿namespace _01_variaveis_e_tipos_de_dados._02_tipos_de_dados_personalizados;
+
+public interface IShape
+{
+    double GetArea();
+    double GetPerimeter();
+}
