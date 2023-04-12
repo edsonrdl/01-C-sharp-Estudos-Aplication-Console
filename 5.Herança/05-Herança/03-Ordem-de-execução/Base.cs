0d08f30336ebb02cf0bc1db739127a90eb51128b
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2023-04-12 10:13:54 -0300

    feat:Corrigido nome

---
+++ 5.Herança/05-Herança/03-Ordem-de-execução/Base.cs
@@ -0,0 +1,25 @@
+﻿namespace _05_Herança._03_Ordem_de_execução
+{
+    public class Base
+    {
+        public void ExibiResultado()
+        {
+            Console.WriteLine("Base");
+        }
+    }
+
+    public class Devirada1 : Base
+    {
+        public void ExibiResultado()
+        {
+            Console.WriteLine("Derivada 1");
+        }
+    }
+    public class Devirada2 : Devirada1
+    {
+        public void ExibiResultado()
+        {
+            Console.WriteLine("Derivada 2");
+        }
+    }
+}
--- 5.Herança/05-Herança/Program.cs
+++ 5.Herança/05-Herança/Program.cs
@@ -1,5 +1,4 @@
-﻿using _05_Herança._02_Heranca_de_construtores;
-var caminhaoTest = new Caminhao("modelo", "marca", 2010, (float)30.00);
-var carroTeste = new Carro("modelo", "marca", 2010, (float)30.00, 4);
-carroTeste.MostrarInformacoes();
-caminhaoTest.MostrarInformacoes();
+﻿
+using _05_Herança._03_Ordem_de_execução;
+
+var teste =new Devirada2();
