52e5b407cda53ee978747f3d1540d285b3b44ef2
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2023-04-12 00:01:52 -0300

    feat:Criado herança

---
+++ 5.Herança/05-Herança/01-Declaracao-de-classe-base-derivada/Veiculo.cs
@@ -0,0 +1,41 @@
+﻿namespace _05_Herança._01_Declaracao_de_classe_base_derivada
+{
+     public class Veiculo
+    {
+        // Atributos
+        public string Modelo;
+        public string Marca;
+        public int Ano;
+        public float Preco;
+        public bool Ligado;
+
+        // Métodos
+        public void Ligar()
+        {
+            Ligado=true;
+        }
+
+        public void Desligado()
+        {
+            Ligado = false;
+
+        }
+
+        public void MostrarInformacoes()
+        {
+            Console.WriteLine($"Modelo: {Modelo}\nMarca: {Marca}\nAno: {Ano}\nPreço: {Preco:C}");
+        }
+
+       
+    }
+
+     //class Carro devira da class veiculo que é a base
+    public class Carro : Veiculo 
+     {
+         public void CarroTeste()
+         {
+             Ligado = true;
+         }
+
+     }
+}
---
+++ 5.Herança/05-Herança/Program.cs
@@ -0,0 +1,5 @@
+﻿using _05_Herança._02_Heranca_de_construtores;
+var caminhaoTest = new Caminhao("modelo", "marca", 2010, (float)30.00);
+var carroTeste = new Carro("modelo", "marca", 2010, (float)30.00, 4);
+carroTeste.MostrarInformacoes();
+caminhaoTest.MostrarInformacoes();
