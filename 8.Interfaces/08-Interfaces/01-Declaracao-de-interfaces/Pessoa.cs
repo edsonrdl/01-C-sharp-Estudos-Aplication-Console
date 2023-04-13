f3115017abcd93f5f5d5034b3b49febb5819938e
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2023-04-13 00:09:35 -0300

    feat:Criado interface

---
+++ 8.Interfaces/08-Interfaces/01-Declaracao-de-interfaces/Pessoa.cs
@@ -0,0 +1,68 @@
+﻿namespace _08_Interfaces._01_Declaracao_de_interfaces
+{
+    public interface  IPessoa
+    {
+
+        void Nome();
+        void Idade();
+     
+    }
+    public interface IEndereco
+    {
+        void Logradouro();
+        void Numero();
+        void Cidade();
+        void Estado();
+    }
+   
+    public class Pessoa : IPessoa, IEndereco
+    {
+        public string nome;
+        public int idade;
+        public string logradouro;
+        public string numero;
+        public string cidade;
+        public string estado;
+      
+        public Pessoa(string nome, int idade, string logradouro, string numero, string cidade, string estado)
+        {
+            this.nome = nome;
+            this.idade = idade;
+            this.logradouro = logradouro;
+            this.numero = numero;
+            this.cidade = cidade;
+            this.estado = estado;
+        }
+
+        public void Nome()
+        {
+            Console.WriteLine(this.nome = "João");
+        }
+
+        public void Idade()
+        {
+            Console.WriteLine(this.idade = 40);
+        }
+        public void Logradouro()
+        { 
+            Console.WriteLine(this.logradouro = "Rua A");
+        }
+        public void Numero()
+        {
+            Console.WriteLine(this.numero = "123");
+           
+        }
+        public void Cidade()
+        {
+            Console.WriteLine(this.cidade = "São Paulo");
+            
+        }
+        public void Estado()
+        {
+            Console.Write(this.cidade = "São Paulo");
+
+        }
+
+    }
+
+}
---
+++ 8.Interfaces/08-Interfaces/Program.cs
@@ -0,0 +1,11 @@
+﻿ using _08_Interfaces._01_Declaracao_de_interfaces;
+
+ Pessoa pessoa = new Pessoa("",0,"","","","");
+
+
+     pessoa.Nome();
+     pessoa.Idade();
+     pessoa.Logradouro();
+     pessoa.Numero();
+     pessoa.Cidade();
+     pessoa.Estado();
