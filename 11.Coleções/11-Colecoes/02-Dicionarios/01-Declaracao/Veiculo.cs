bb4f8320020e341e7e2022037abdcab5d9230946
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2023-04-15 23:53:33 -0300

    feat:Criado coleção Dicionário

---
+++ 11.Coleções/11-Colecoes/02-Dicionarios/01-Declaracao/Veiculo.cs
@@ -0,0 +1,23 @@
+﻿namespace _11_Colecoes._02_Dicionarios._01_Declaracao
+{
+     class Veiculo
+    {
+        public void CriarVeiculo()
+        {
+            Dictionary<int, string> veiculo = new Dictionary<int, string>();
+            veiculo.Add(12, "Caminhão");
+            veiculo.Add(4, "Carro");
+            veiculo.Add(6, "Avião");
+            veiculo.Add(2, "Motocicleta"); 
+
+             Console.WriteLine("Exibir :{0}",veiculo.Count);
+
+             Dictionary<int, string>.ValueCollection valores = veiculo.Values;
+             foreach (var v in veiculo)
+             {
+                Console.WriteLine(v);
+
+            }
+        }
+    }
+}
