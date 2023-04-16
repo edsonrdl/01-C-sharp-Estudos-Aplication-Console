82461af053fb7390b99fed703be0e49d7100d6e6
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2023-04-16 00:08:38 -0300

    feat:Criado coleções

---
+++ 11.Coleções/11-Colecoes/04-Queue/01-Declaracao/Veiculo.cs
@@ -0,0 +1,50 @@
+﻿namespace _11_Colecoes._04_Queue
+{
+     class Veiculo
+    {
+        public void QueueDeclaracao()
+        {
+            string[] v = new[] { "Caminhão", "Carro" };
+            Queue<string> veiculo = new Queue<string>(v);
+            Console.WriteLine("Quantidade de :{0}", veiculo.Count);
+
+
+        }
+        public void QueueEnQueue()
+        { //Enqueue adiciona na Fila o item sempre no final
+            string[] v = new[] { "Caminhão", "Carro" };
+            Queue<string> veiculo = new Queue<string>(v);
+
+            veiculo.Enqueue("Navio");
+
+            Console.WriteLine("Quantidade de :{0}", veiculo.Count);
+            foreach (var v1 in veiculo)
+            {
+                Console.WriteLine(v1);
+            }
+            
+
+
+        } 
+        public void QueueDeQueue()
+        { //Enqueue adiciona na Fila o item sempre no final
+            string[] v = new[] { "Caminhão", "Carro" };
+            Queue<string> veiculo = new Queue<string>(v);
+
+
+            Console.WriteLine("Quantidade de :{0}", veiculo.Count);
+            foreach (var v1 in veiculo)
+            {
+                Console.WriteLine(v1);
+            }
+
+            veiculo.Dequeue();
+
+            Console.WriteLine("Quantidade de :{0}", veiculo.Count);
+            foreach (var v1 in veiculo)
+            {
+                Console.WriteLine(v1);
+            }
+        }
+    }
+}
---
+++ 11.Coleções/11-Colecoes/Program.cs
@@ -0,0 +1,5 @@
+﻿
+ using _11_Colecoes._04_Queue;
+
+ var teste = new Veiculo();
+ teste.QueueDeQueue();
